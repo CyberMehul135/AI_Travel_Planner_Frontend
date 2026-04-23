9475146ff58d9fbaef244c1b7e0f7382474aaa21
CyberMehul <mehulrathod9254@gmail.com>
2026-04-23 18:29:54 +0530

    style: improve active state UI in mobile bottom navigation

--- src/components/common/MobileBottomNav.jsx
+++ src/components/common/MobileBottomNav.jsx
@@ -16,54 +16,64 @@ export const MobileBottomNav = () => {
     {
       to: "/",
       icon: ChartColumnIncreasing,
+      title: "Home",
     },
     {
       to: "/trips/create",
       icon: Plane,
+      title: "Planning",
     },
     {
       to: "/trips",
       icon: Planner,
+      title: "Trips",
     },
     {
       to: "/user/profile",
       icon: CircleUser,
+      title: "You",
     },
   ];
 
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 w-full max-w-md px-2 z-50 md:hidden">
-      <div className="flex justify-between items-center bg-background/80 backdrop-blur-xl border rounded-2xl px-4 py-2 shadow-lg">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-md px-0 z-50 md:hidden">
+      <div className="flex justify-around items-center bg-background/80 backdrop-blur-xl border py-2 shadow-lg">
         {/* Navigation Icons */}
         {navItems.map((item, index) => {
           const Icon = item.icon;
 
           return (
-            <NavLink
-              key={index}
-              to={item.to}
-              end={item.to === "/trips"}
-              className={({ isActive }) =>
-                cn(
-                  "p-2 rounded-xl transition-all duration-200",
-                  isActive
-                    ? " text-white bg-primary"
-                    : "dark:text-white hover:text-primary",
-                )
-              }
-            >
-              <Icon size={22} />
-            </NavLink>
+            <div className="flex flex-col items-center">
+              <NavLink
+                key={index}
+                to={item.to}
+                end={item.to === "/trips"}
+                className={({ isActive }) =>
+                  cn(
+                    "p-2 rounded-xl transition-all duration-200",
+                    isActive
+                      ? "text-white bg-primary"
+                      : "dark:text-white hover:text-primary",
+                  )
+                }
+              >
+                <Icon size={22} />
+              </NavLink>
+              <p className="text-xs mt-0.5">{item.title}</p>
+            </div>
           );
         })}
 
         {/* Sidebar Toggle */}
-        <button
-          onClick={toggleSidebar}
-          className="p-2 rounded-xl hover:text-primary transition dark:text-white text-black"
-        >
-          <Menu size={22} />
-        </button>
+        <div className="flex flex-col items-center">
+          <button
+            onClick={toggleSidebar}
+            className="p-2 rounded-xl hover:text-primary transition dark:text-white text-black"
+          >
+            <Menu size={22} />
+          </button>
+          <p className="text-xs mt-0.5">Menu</p>
+        </div>
       </div>
     </div>
   );
